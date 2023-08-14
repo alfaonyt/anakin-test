8713a0e282932b20d1a14b752f6a6a98fbea40f1
alfaonyt <alex@pillou.net>
2023-08-14 13:58:20 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/asus/ASUS_I005_1
+DEVICE_PATH := device/asus/anakin
+
+include build/make/target/board/BoardConfigMainlineCommon.mk
 
 # A/B
 AB_OTA_UPDATER := true
@@ -21,6 +23,15 @@ AB_OTA_PARTITIONS := \
     vendor_dlkm \
     vendor_boot
 
+# AVB
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+
+BOARD_AVB_VBMETA_SYSTEM := product system system_ext
+BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA4096
+BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 2
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
@@ -36,23 +47,64 @@ TARGET_2ND_CPU_VARIANT := cortex-a76
 # APEX
 DEXPREOPT_GENERATE_APEX_IMAGE := true
 
+# Audio
+AUDIO_FEATURE_ENABLED_DLKM := true
+AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT := true
+AUDIO_FEATURE_ENABLED_EXT_AMPLIFIER := true
+AUDIO_FEATURE_ENABLED_GEF_SUPPORT := true
+AUDIO_FEATURE_ENABLED_GKI := true
+AUDIO_FEATURE_ENABLED_INSTANCE_ID := true
+AUDIO_FEATURE_ENABLED_PROXY_DEVICE := true
+AUDIO_FEATURE_ENABLED_SSR := true
+AUDIO_FEATURE_ENABLED_SVA_MULTI_STAGE := true
+BOARD_SUPPORTS_OPENSOURCE_STHAL := true
+BOARD_SUPPORTS_SOUND_TRIGGER := true
+BOARD_USES_ALSA_AUDIO := true
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := lahaina
 TARGET_NO_BOOTLOADER := true
 
+# DTB
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_BOOT_HEADER_VERSION := 4
+BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOT_HEADER_VERSION)
+
+# DTBO
+BOARD_KERNEL_SEPARATED_DTBO := true
+
 # Display
+SOONG_CONFIG_qtidisplay_udfps := true
 TARGET_SCREEN_DENSITY := 420
 
 # Kernel
-BOARD_BOOTIMG_HEADER_VERSION := 3
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=0 loop.max_part=7 cgroup.memory=nokmem,nosocket pcie_ports=compat loop.max_part=7 iptable_raw.raw_before_defrag=1 ip6table_raw.raw_before_defrag=1 buildvariant=user
+BOARD_KERNEL_CMDLINE := \
+    androidboot.console=ttyMSM0 \
+    androidboot.hardware=qcom \
+    androidboot.memcg=1 \
+    androidboot.usbcontroller=a600000.dwc3 \
+    cgroup.memory=nokmem,nosocket \
+    console=ttyMSM0,115200n8 \
+    ip6table_raw.raw_before_defrag=1 \
+    iptable_raw.raw_before_defrag=1 \
+    loop.max_part=7 \
+    lpm_levels.sleep_disabled=1 \
+    msm_rtb.filter=0x237 \
+    pcie_ports=compat \
+    service_locator.enable=1 \
+    swiotlb=0 \
+    kpti=off
+
+BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_RAMDISK_USE_LZ4 := true
+
 BOARD_KERNEL_IMAGE_NAME := Image
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-BOARD_KERNEL_SEPARATED_DTBO := true
-TARGET_KERNEL_CONFIG := ASUS_I005_1_defconfig
-TARGET_KERNEL_SOURCE := kernel/asus/ASUS_I005_1
+BOARD_USES_GENERIC_KERNEL_IMAGE := true
+#TARGET_KERNEL_CONFIG := vendor/$(PRODUCT_DEVICE)_defconfig # Prebuilt kernel for now
+TARGET_KERNEL_SOURCE := kernel/asus/sm8350
+TARGET_KERNEL_NO_GCC := true
 
 # Kernel - prebuilt
 TARGET_FORCE_PREBUILT_KERNEL := true
@@ -68,6 +120,95 @@ endif
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/prebuilts/modules/vendor,$(TARGET_COPY_OUT_VENDOR)/lib/modules) \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/prebuilts/modules/vendor_boot,$(TARGET_COPY_OUT_VENDOR_BOOT)/lib/modules)
+
+BOARD_VENDOR_KERNEL_MODULES_LOAD := \ # TODO: Fix prebuilt kernel modules
+    adsp_loader_dlkm.ko \
+    apr_dlkm.ko \
+    aw8697.ko \
+    bolero_cdc_dlkm.ko \
+    bt_fm_slim.ko \
+    btpower.ko \
+    camera.ko \
+    cs35l45_i2c_dlkm.ko \
+    e4000.ko \
+    fc0011.ko \
+    fc0012.ko \
+    fc0013.ko \
+    fc2580.ko \
+    focaltech_fts_zf.ko \
+    gf_spi.ko \
+    hdmi_dlkm.ko \
+    hid-aksys.ko \
+    icm206xx.ko \
+    it913x.ko \
+    llcc_perfmon.ko \
+    m88rs6000t.ko \
+    machine_dlkm.ko \
+    max2165.ko \
+    mbhc_dlkm.ko \
+    mc44s803.ko \
+    msi001.ko \
+    msm_drm.ko \
+    mt2060.ko \
+    mt2063.ko \
+    mt20xx.ko \
+    mt2131.ko \
+    mt2266.ko \
+    mxl301rf.ko \
+    mxl5005s.ko \
+    mxl5007t.ko \
+    native_dlkm.ko \
+    pinctrl_lpi_dlkm.ko \
+    pinctrl_wcd_dlkm.ko \
+    platform_dlkm.ko \
+    q6_dlkm.ko \
+    q6_notifier_dlkm.ko \
+    q6_pdr_dlkm.ko \
+    qcom_edac.ko \
+    qm1d1b0004.ko \
+    qm1d1c0042.ko \
+    qt1010.ko \
+    r820t.ko \
+    rdbg.ko \
+    rmnet_core.ko \
+    rmnet_ctl.ko \
+    rmnet_offload.ko \
+    rmnet_shs.ko \
+    rx_macro_dlkm.ko \
+    sensors_vcnl36866.ko \
+    si2157.ko \
+    slimbus.ko \
+    slimbus-ngd.ko \
+    snd_event_dlkm.ko \
+    stub_dlkm.ko \
+    swr_ctrl_dlkm.ko \
+    swr_dlkm.ko \
+    swr_dmic_dlkm.ko \
+    swr_haptics_dlkm.ko \
+    tda18212.ko \
+    tda18218.ko \
+    tda18250.ko \
+    tda9887.ko \
+    tea5761.ko \
+    tea5767.ko \
+    tua9001.ko \
+    tuner-simple.ko \
+    tuner-types.ko \
+    tuner-xc2028.ko \
+    tx_macro_dlkm.ko \
+    va_macro_dlkm.ko \
+    wcd937x_dlkm.ko \
+    wcd937x_slave_dlkm.ko \
+    wcd938x_dlkm.ko \
+    wcd938x_slave_dlkm.ko \
+    wcd9xxx_dlkm.ko \
+    wcd_core_dlkm.ko \
+    wsa883x_dlkm.ko \
+    wsa_macro_dlkm.ko \
+    xc4000.ko \
+    xc5000.ko
+
+TARGET_MODULE_ALIASES += wlan.ko:qca_cld3_wlan.ko # TODO: Delete if not necessary
 
 # Partitions
 BOARD_ASUS_DYNAMIC_PARTITIONS_PARTITION_LIST := odm product system system_ext vendor vendor_dlkm
@@ -103,20 +244,25 @@ TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 
 # Recovery
+BOARD_INCLUDE_RECOVERY_DTBO := true
+BOARD_INCLUDE_RECOVERY_RAMDISK_IN_VENDOR_BOOT := true
+BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
+BOOT_KERNEL_MODULES := \ # TODO: Fix modules
+    focaltech_fts_zf.ko \
+    msm_drm.ko \
+    sensors_vcnl36866.ko
+
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(BOOT_KERNEL_MODULES)
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.emmc
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
-TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_RECOVERY_UI_MARGIN_HEIGHT := 100
 TARGET_USERIMAGES_USE_F2FS := true
 
 # Security patch level
 VENDOR_SECURITY_PATCH := 2021-08-01
 
-# Verified Boot
-BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
-
 # VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Inherit the proprietary files
-include vendor/asus/ASUS_I005_1/BoardConfigVendor.mk
+include vendor/asus/anakin/BoardConfigVendor.mk
